86b2c16c7e355a089203f98fe3dd1f477e468537
yangqingpinggit <1226859780@qq.com>
2020-10-14 13:05:51 +0800

    [A]新增func3

---
+++ ActivityServicelmpl.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+add func1
+@Service()
+public class ActivityServiceImpl{
+	add func2
+	@Override
+	public void deleteAnswerDetail(){
+	}
+
+	add func3
+	@Override
+	public void updateNewsActivityNew(){
+	}
+
+}
+
